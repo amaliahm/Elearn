3f4d150f0534c5930a444bbe238dddd4a4dd1c79
amalia <a.ghandouz@esi-sba.dz>
2024-04-24 09:06:45 -0400

    fix pc

--- src/components/landing page/hero.jsx
+++ src/components/landing page/hero.jsx
@@ -6,7 +6,7 @@ const HeroComponent = () => {
 
     return (
         <>
-          <div className="app__header w-full app__wrapper section__padding" id="home">
+          <div className="app__bg w-full app__wrapper section__padding" id="home">
             <div className="app__wrapper_info">
               <h1 className="app__header-h1">Lorem ipsum dolor sit amet.</h1>
               <p className="p__opensans" style={{ margin: '2rem 0' }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rem assumenda beatae tenetur, velit porro ducimus illo, recusandae, magni facere iure minima magnam tempore rerum. </p>
