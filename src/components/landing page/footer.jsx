e473c867e36d8fa79e23e38bb1ce974b67894090
amalia <a.ghandouz@esi-sba.dz>
2024-04-26 11:41:02 -0400

    add footer

--- src/components/landing page/footer.jsx
+++ src/components/landing page/footer.jsx
@@ -1,8 +1,11 @@
 const FooterComponent = () => {
     return (
-        <>
-          <h1>FooterComponent</h1>
-        </>
+        <div className="app__bg">
+          <div className="footer">
+            <a href="https://www.esi-sba.dz/fr/">esi-sba</a>
+            <h1>© ECOLE SUPERIEURE EN INFORMATIQUE SIDI BEL ABBES 2024</h1>
+          </div>
+        </div>
     )
 }
 
