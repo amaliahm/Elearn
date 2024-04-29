16a49069cba15067249e7bb3e87a5c07b0dc79bd
amalia <a.ghandouz@esi-sba.dz>
2024-04-29 09:08:40 -0400

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import LandingPageComponent from './components/landing page/LandingPage.jsx';
 import HomeComponentAdmin from './container/HomeAdmin.jsx';
 import HomeComponentUser from './container/HomeUser.jsx';
 import ProfileComponent from './container/Profile.jsx';
-import './css/App.css';
 
 
 function App() {
@@ -19,11 +18,11 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
-    }, 2000);
+    }, 1000);
   }, [])
 
   return (
-    <>
+    <div className='container-xxl bg-white p-0'>
       { isLoading ? <LoadingComponent /> : <Router>
         <Routes>
           {/* <Route path="*" element={<PageNotFound />} /> */}
@@ -44,7 +43,7 @@ function App() {
           <Route path='/profile' element={<ProfileComponent />} />
         </Routes>
       </Router>}
-    </>
+    </div>
   )
 }
 
--- src/Layouts/Loading.jsx
+++ src/Layouts/Loading.jsx
@@ -1,11 +1,8 @@
 const LoadingComponent = () => {
     return (
-        <div className="loading">
-            <div className="load">
-                <div className="upper ball"></div>
-                <div className="lower ball"></div>
-                <div className="left ball"></div>
-                <div className="right ball"></div>
+        <div id='spinner' className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div className="spinner-grow text-primary" style={{width: "3rem", height: "3rem"}} role="status">
+                <span className="sr-only">Loading...</span>
             </div>
         </div>
     )
