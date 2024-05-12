e4a0bdf62ee4f59b6d2feb050e648fbd26c3c0bc
amalia <a.ghandouz@esi-sba.dz>
2024-05-12 05:59:56 -0400

    fix home.user

--- src/container/HomeUser.jsx
+++ src/container/HomeUser.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import NavBar from '../components/landing page/NavBar'
 import FooterComponent from '../components/landing page/footer'
-import { navBarElementsUser } from "../constants/data"
+import { navBarElementsUser } from '../constants/data'
+import Home from './Home'
+import NavBarComponent from './NavBarComponent'
 
 const HomeComponentUser = () => {
     return (
         <>
-          <div>
-            <NavBar elements={navBarElementsUser} />
-              
+          <div className='container-xxl bg-white p-0'>
+            <NavBarComponent elements={navBarElementsUser} home={true} />
+            <main>
+              <div className='container-xxl bg-primary p-0 mb-5'>
+                <Home user='User' />
+              </div>
+            </main>
             <FooterComponent />
           </div>
         </>
