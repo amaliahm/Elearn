f3a7fda9261dbb7069d827608bc9b04adb5ffee2
amalia <a.ghandouz@esi-sba.dz>
2024-05-18 18:13:48 -0400

    fix cohorte --name

--- src/components/communication/Cohorte.jsx
+++ src/components/communication/Cohorte.jsx
@@ -14,6 +14,9 @@ const Cohorte = () => {
     return (
         <>
           <div className='container-xxl bg-white p-0'>
+            <div className="absolute top-7 left-5 selected-answer rounded-2xl px-3 py-1">
+                <p>Cohorte-1CS 2023-2024</p>
+            </div>
             <div className="absolute top-1 right-5">
                 <Button className="add-comm" style={{color: 'white'}} >
                     <FaQuestion onClick={() => navigate(`${location.pathname}/add`)}/>
@@ -24,15 +27,12 @@ const Cohorte = () => {
             </div>
             <main>
                 <div className="container-xxl p-0 mb-5">
-                    <div className="container-xxl py-5">
+                    <div className="container-xxl py-4">
                         <div className="container px-lg-5">
-                            <div className='section-title position-relative text-center mb-5 mt-0 pb-2 wow fadeInUp' data-wow-delay="0.1s">
+                            <div className=' position-relative text-center mb-5 mt-2 pb-2 wow fadeInUp' data-wow-delay="0.1s">
                                 <h6 className="position-relative d-inline text-primary ps-4">
                                     Communications
                                 </h6>
-                                <h1 className='mt-2'>
-                                    Cohorte-1CS 2023-2024
-                                </h1>
                             </div>
                         </div>
                         <Accordion defaultActiveKey={['0']}  >
