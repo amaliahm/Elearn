1ba3f028f12358e0a86ad7b33dba0fd2b4968201
amalia <a.ghandouz@esi-sba.dz>
2024-05-19 09:58:40 -0400

    add quiz.participate->time --f

--- src/components/quiz/TimeComponent.jsx
+++ src/components/quiz/TimeComponent.jsx
@@ -1,80 +1,32 @@
 import React, { useState } from "react";
-
-function ControlButtons(props) {
-    const StartButton = (
-        <div className="btn btn-one btn-start"
-            onClick={props.handleStart}>
-            Start
-        </div>
-    );
-    const ActiveButtons = (
-        <div className="btn-grp">
-            <div className="btn btn-two"
-                onClick={props.handleReset}>
-                Reset
-            </div>
-            <div className="btn btn-one">
-                {"Resume"}
-            </div>
-        </div>
-    );
- 
-    return (
-        <div className="Control-Buttons">
-            <div>{props.active ? ActiveButtons : StartButton}</div>
-        </div>
-    );
-}
-
-function Timer(props) {
-    return (
-        <div className="text-black">
-            <span className="digits">
-                {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
-            </span>
-            <span className="digits">
-                {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
-            </span>
-        </div>
-    );
-}
+import { useNavigate } from "react-router-dom";
 
 function TimeComponent() {
-    const [isActive, setIsActive] = useState(true);
-    const [time, setTime] = useState(15);
+    const [time, setTime] = useState(10000);
+    const navigate = useNavigate()
  
     React.useEffect(() => {
         let interval = null;
-        if (isActive === false) {
-            interval = setInterval(() => {
-                setTime((time) => time + 10);
-            }, 10);
-        } else {
+        interval = setInterval(() => {
+            setTime((time) => time - 10);
+        }, 10);
+        if (time === 0) {
             clearInterval(interval);
+            navigate('/home/user/assessment')
         }
         return () => {
             clearInterval(interval);
         };
-    }, [isActive]);
- 
-    const handleStart = () => {
-        
-    };
- 
- 
-    const handleReset = () => {
-        setIsActive(false);
-        setTime(0);
-    };
+    },);
  
     return (
-        <div>
-            <Timer time={time} />
-            <ControlButtons
-                active={isActive}
-                handleStart={handleStart}
-                handleReset={handleReset}
-            />
+        <div className="">
+            <span className="text-3xl text-[var(--main-color)]">
+                {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span className="text-3xl text-[var(--main-color)]">
+                {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
+            </span>
         </div>
     );
 }
