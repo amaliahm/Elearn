9d345dbaa48c231300e8fa94e4030f361511ab6c
amalia <a.ghandouz@esi-sba.dz>
2024-06-05 10:46:29 -0400

    update landing-page.modules 1

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,14 +1,22 @@
 import ACED_Mohammed_Reda from './ACED_Mohammed_Reda.png'
 import NadiaElouali from './NadiaElouali.png'
+import acsi from './acsi.jpg'
+import analyse from './analyse.jpg'
+import archi from './archi.png'
 import azza_mohammed from './azza_mohammed.png'
+import bdd from './bdd.jpg'
 import bg from './bg.png'
+import cloud_computing from './cloud-computing.jpg'
 import cohorte from './cohorte.jpg'
 import course from './course.png'
 import education from './education.png'
 import education1 from './education1.png'
 import HeroImage from './hero.png'
+import reseau from './reseau.jpg'
+import se from './se.png'
+import thl from './thl.jpg'
 
 export {
-    ACED_Mohammed_Reda, HeroImage, NadiaElouali, azza_mohammed, bg, cohorte, course, education, education1
+    ACED_Mohammed_Reda, HeroImage, NadiaElouali, acsi, analyse, archi, azza_mohammed, bdd, bg, cloud_computing, cohorte, course, education, education1, reseau, se, thl
 }
 
